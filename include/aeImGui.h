5887362cbf5100b6f889c597c9a5048f46c0cc20
John Hughes <hughesj17@gmail.com>
2020-05-23 19:50:44 -0700

    Added InitializeHeadless() to aeImGui.

--- include/aeImGui.h
+++ include/aeImGui.h
@@ -34,56 +34,43 @@
 	#define IMGUI_IMPL_OPENGL_LOADER_GLEW
 #elif _AE_APPLE_
 	#define IMGUI_IMPL_OPENGL_LOADER_CUSTOM <OpenGL/gl3.h>
+#elif _AE_LINUX_
+	#define IMGUI_IMPL_OPENGL_LOADER_CUSTOM <GLES3/gl3.h>
 #endif
 #include "imgui_impl_opengl3.h"
 
 //------------------------------------------------------------------------------
 // Imgui helpers
 //------------------------------------------------------------------------------
-namespace aeImGui
+class aeImGui
 {
-	inline void Initialize()
+public:
+	void Initialize()
 	{
-		ImGui::CreateContext();
-		// ImGuiIO& io = ImGui::GetIO(); (void)io;
-		// io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-		// io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-		ImGui::StyleColorsDark();
-		ImGui_ImplOpenGL3_Init( "#version 330" );
-
-		ImGuiIO& io = ImGui::GetIO();
-		io.KeyMap[ ImGuiKey_Tab ] = (uint32_t)aeKey::Tab;
-		io.KeyMap[ ImGuiKey_LeftArrow ] = (uint32_t)aeKey::Left;
-		io.KeyMap[ ImGuiKey_RightArrow ] = (uint32_t)aeKey::Right;
-		io.KeyMap[ ImGuiKey_UpArrow ] = (uint32_t)aeKey::Up;
-		io.KeyMap[ ImGuiKey_DownArrow ] = (uint32_t)aeKey::Down;
-		io.KeyMap[ ImGuiKey_PageUp ] = (uint32_t)aeKey::PageUp;
-		io.KeyMap[ ImGuiKey_PageDown ] = (uint32_t)aeKey::PageDown;
-		io.KeyMap[ ImGuiKey_Home ] = (uint32_t)aeKey::Home;
-		io.KeyMap[ ImGuiKey_End ] = (uint32_t)aeKey::End;
-		io.KeyMap[ ImGuiKey_Insert ] = (uint32_t)aeKey::Insert;
-		io.KeyMap[ ImGuiKey_Delete ] = (uint32_t)aeKey::Delete;
-		io.KeyMap[ ImGuiKey_Backspace ] = (uint32_t)aeKey::Backspace;
-		io.KeyMap[ ImGuiKey_Space ] = (uint32_t)aeKey::Space;
-		io.KeyMap[ ImGuiKey_Enter ] = (uint32_t)aeKey::Enter;
-		io.KeyMap[ ImGuiKey_Escape ] = (uint32_t)aeKey::Escape;
-		io.KeyMap[ ImGuiKey_KeyPadEnter ] = (uint32_t)aeKey::NumPadEnter;
-		io.KeyMap[ ImGuiKey_A ] = (uint32_t)aeKey::A;
-		io.KeyMap[ ImGuiKey_C ] = (uint32_t)aeKey::C;
-		io.KeyMap[ ImGuiKey_V ] = (uint32_t)aeKey::V;
-		io.KeyMap[ ImGuiKey_X ] = (uint32_t)aeKey::X;
-		io.KeyMap[ ImGuiKey_Y ] = (uint32_t)aeKey::Y;
-		io.KeyMap[ ImGuiKey_Z ] = (uint32_t)aeKey::Z;
+		m_Initialize();
 	}
 
-	inline void Terminate()
+	// @NOTE: Allows imgui functions to be called in a windowless application
+	void InitializeHeadless()
 	{
-		ImGui_ImplOpenGL3_Shutdown();
+		m_headless = true;
+		m_Initialize();
+	}
+
+	void Terminate()
+	{
+		AE_ASSERT( m_init );
+		if ( !m_headless )
+		{
+			ImGui_ImplOpenGL3_Shutdown();
+		}
 		ImGui::DestroyContext();
 	}
 
-	inline void NewFrame( aeRender* render, aeInput* input )
+	void NewFrame( aeRender* render, aeInput* input )
 	{
+		AE_ASSERT( m_init );
+
 		const InputState* inputState = input->GetState();
 		ImGuiIO& io = ImGui::GetIO();
 
@@ -115,15 +102,74 @@ namespace aeImGui
 
 		ImGui::GetIO().DisplaySize = ImVec2( render->GetWidth(), render->GetHeight() );
 		
-		ImGui_ImplOpenGL3_NewFrame();
+		if ( !m_headless )
+		{
+			ImGui_ImplOpenGL3_NewFrame();
+		}
 		ImGui::NewFrame();
 	}
 
-	inline void Render()
+	void Render()
 	{
+		AE_ASSERT( m_init );
 		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData( ImGui::GetDrawData() );
+		if ( !m_headless )
+		{
+			ImGui_ImplOpenGL3_RenderDrawData( ImGui::GetDrawData() );
+		}
 	}
-}
+private:
+	void m_Initialize()
+	{
+		AE_ASSERT( !m_init );
+
+		ImGui::CreateContext();
+		// ImGuiIO& io = ImGui::GetIO(); (void)io;
+		// io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+		// io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+		ImGui::StyleColorsDark();
+
+		if ( m_headless )
+		{
+			ImGuiIO& io = ImGui::GetIO();
+			unsigned char* pixels;
+			int width, height;
+			io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
+		}
+		else
+		{
+			ImGui_ImplOpenGL3_Init( "#version 330" );
+		}
+
+		ImGuiIO& io = ImGui::GetIO();
+		io.KeyMap[ ImGuiKey_Tab ] = (uint32_t)aeKey::Tab;
+		io.KeyMap[ ImGuiKey_LeftArrow ] = (uint32_t)aeKey::Left;
+		io.KeyMap[ ImGuiKey_RightArrow ] = (uint32_t)aeKey::Right;
+		io.KeyMap[ ImGuiKey_UpArrow ] = (uint32_t)aeKey::Up;
+		io.KeyMap[ ImGuiKey_DownArrow ] = (uint32_t)aeKey::Down;
+		io.KeyMap[ ImGuiKey_PageUp ] = (uint32_t)aeKey::PageUp;
+		io.KeyMap[ ImGuiKey_PageDown ] = (uint32_t)aeKey::PageDown;
+		io.KeyMap[ ImGuiKey_Home ] = (uint32_t)aeKey::Home;
+		io.KeyMap[ ImGuiKey_End ] = (uint32_t)aeKey::End;
+		io.KeyMap[ ImGuiKey_Insert ] = (uint32_t)aeKey::Insert;
+		io.KeyMap[ ImGuiKey_Delete ] = (uint32_t)aeKey::Delete;
+		io.KeyMap[ ImGuiKey_Backspace ] = (uint32_t)aeKey::Backspace;
+		io.KeyMap[ ImGuiKey_Space ] = (uint32_t)aeKey::Space;
+		io.KeyMap[ ImGuiKey_Enter ] = (uint32_t)aeKey::Enter;
+		io.KeyMap[ ImGuiKey_Escape ] = (uint32_t)aeKey::Escape;
+		io.KeyMap[ ImGuiKey_KeyPadEnter ] = (uint32_t)aeKey::NumPadEnter;
+		io.KeyMap[ ImGuiKey_A ] = (uint32_t)aeKey::A;
+		io.KeyMap[ ImGuiKey_C ] = (uint32_t)aeKey::C;
+		io.KeyMap[ ImGuiKey_V ] = (uint32_t)aeKey::V;
+		io.KeyMap[ ImGuiKey_X ] = (uint32_t)aeKey::X;
+		io.KeyMap[ ImGuiKey_Y ] = (uint32_t)aeKey::Y;
+		io.KeyMap[ ImGuiKey_Z ] = (uint32_t)aeKey::Z;
+
+		m_init = true;
+	}
+
+	bool m_init = false;
+	bool m_headless = false;	
+};
 
 #endif
