f5df4895ac276979d0e35041aa517173cf8fde65
John Hughes <hughesj17@gmail.com>
2020-08-05 09:51:56 -0700

    Only report opengl debug messages that are medium or high severity.

--- src/aeOpenGLRender.cpp
+++ src/aeOpenGLRender.cpp
@@ -146,50 +146,51 @@ void aeOpenGLDebugCallback( GLenum source,
   const GLchar* message,
   const void* userParam )
 {
-  AE_INFO( message );
+  //std::cout << "---------------------opengl-callback-start------------" << std::endl;
+  //std::cout << "message: " << message << std::endl;
+  //std::cout << "type: ";
+  //switch ( type )
+  //{
+  //  case GL_DEBUG_TYPE_ERROR:
+  //    std::cout << "ERROR";
+  //    break;
+  //  case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+  //    std::cout << "DEPRECATED_BEHAVIOR";
+  //    break;
+  //  case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+  //    std::cout << "UNDEFINED_BEHAVIOR";
+  //    break;
+  //  case GL_DEBUG_TYPE_PORTABILITY:
+  //    std::cout << "PORTABILITY";
+  //    break;
+  //  case GL_DEBUG_TYPE_PERFORMANCE:
+  //    std::cout << "PERFORMANCE";
+  //    break;
+  //  case GL_DEBUG_TYPE_OTHER:
+  //    std::cout << "OTHER";
+  //    break;
+  //}
+  //std::cout << std::endl;
 
-  std::cout << "---------------------opengl-callback-start------------" << std::endl;
-  std::cout << "message: " << message << std::endl;
-  std::cout << "type: ";
-  switch ( type )
-  {
-    case GL_DEBUG_TYPE_ERROR:
-      std::cout << "ERROR";
-      break;
-    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-      std::cout << "DEPRECATED_BEHAVIOR";
-      break;
-    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-      std::cout << "UNDEFINED_BEHAVIOR";
-      break;
-    case GL_DEBUG_TYPE_PORTABILITY:
-      std::cout << "PORTABILITY";
-      break;
-    case GL_DEBUG_TYPE_PERFORMANCE:
-      std::cout << "PERFORMANCE";
-      break;
-    case GL_DEBUG_TYPE_OTHER:
-      std::cout << "OTHER";
-      break;
-  }
-  std::cout << std::endl;
-
-  std::cout << "id: " << id << std::endl;
-  std::cout << "severity: ";
+  //std::cout << "id: " << id << std::endl;
+  //std::cout << "severity: ";
   switch ( severity )
   {
     case GL_DEBUG_SEVERITY_LOW:
-      std::cout << "LOW";
+      //std::cout << "LOW";
+      //AE_INFO( message );
       break;
     case GL_DEBUG_SEVERITY_MEDIUM:
-      std::cout << "MEDIUM";
+      //std::cout << "MEDIUM";
+      AE_WARN( message );
       break;
     case GL_DEBUG_SEVERITY_HIGH:
-      std::cout << "HIGH";
+      //std::cout << "HIGH";
+      AE_ERR( message );
       break;
   }
-  std::cout << std::endl;
-  std::cout << "---------------------opengl-callback-end--------------" << std::endl;
+  //std::cout << std::endl;
+  //std::cout << "---------------------opengl-callback-end--------------" << std::endl;
 
   if ( severity == GL_DEBUG_SEVERITY_HIGH )
   {
