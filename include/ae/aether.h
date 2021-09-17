d657800388d501588d957d5e09f8b21a672f1369
John Hughes <hughesj17@gmail.com>
2021-09-17 08:35:55 -0700

    Move CollisionMesh

--- include/ae/aether.h
+++ include/ae/aether.h
@@ -2270,6 +2270,90 @@ private:
 };
 
 //------------------------------------------------------------------------------
+// ae::CollisionMesh class
+//------------------------------------------------------------------------------
+class CollisionMesh
+{
+public:
+  // Initialization params
+  struct Params
+  {
+    ae::Matrix4 transform = ae::Matrix4::Identity(); //!< To transform given position values while storing them
+    uint32_t vertexCount = 0;
+    const ae::Vec3* positions = nullptr;
+    uint32_t positionStride = 0;
+    uint32_t indexCount = 0;
+    const uint16_t* indices16 = nullptr;
+  };
+  
+  // RaycastParams
+  struct RaycastParams
+  {
+    ae::Matrix4 transform = ae::Matrix4::Identity();
+    const void* userData = nullptr;
+    ae::Vec3 source = ae::Vec3( 0.0f );
+    ae::Vec3 direction = ae::Vec3( 0.0f, 0.0f, -1.0f );
+    float maxLength = 0.0f;
+    uint32_t maxHits = 1;
+    bool hitCounterclockwise = true;
+    bool hitClockwise = false;
+    ae::DebugLines* debug = nullptr; // Draw collision results
+    ae::Color debugColor = ae::Color::Red();
+  };
+  // RaycastResult
+  struct RaycastResult
+  {
+    uint32_t hitCount = 0;
+    struct Hit
+    {
+      ae::Vec3 position = ae::Vec3( 0.0f );
+      ae::Vec3 normal = ae::Vec3( 0.0f );
+      float distance = 0.0f;
+      const void* userData = nullptr;
+    } hits[ 8 ];
+    
+    static void Accumulate( const RaycastParams& params, const RaycastResult& prev, RaycastResult* next );
+  };
+
+  // Sphere collision PushOutParams
+  struct PushOutParams
+  {
+    ae::Matrix4 transform = ae::Matrix4::Identity();
+    ae::DebugLines* debug = nullptr; // Draw collision results
+    ae::Color debugColor = ae::Color::Red();
+  };
+  // Sphere collision PushOutInfo
+  struct PushOutInfo
+  {
+    ae::Sphere sphere;
+    ae::Vec3 velocity = ae::Vec3( 0.0f );
+    struct Hit
+    {
+      ae::Vec3 position;
+      ae::Vec3 normal;
+    };
+    ae::Array< Hit, 8 > hits;
+
+    static void Accumulate( const PushOutParams& params, const PushOutInfo& prev, PushOutInfo* next );
+  };
+  
+  CollisionMesh( ae::Tag tag );
+  void Load( Params params );
+  void Clear();
+  bool Raycast( const RaycastParams& params, RaycastResult* outResult ) const;
+  RaycastResult Raycast( const RaycastParams& params, const RaycastResult& prevResult ) const;
+  PushOutInfo PushOut( const PushOutParams& params, const PushOutInfo& prevInfo ) const;
+  ae::AABB GetAABB() const { return m_aabb; }
+
+private:
+  typedef uint16_t Index;
+  struct Vertex { ae::Vec4 position; ae::Vec4 normal; };
+  ae::Array< Vertex > m_vertices;
+  ae::Array< Index > m_indices;
+  ae::AABB m_aabb;
+};
+
+//------------------------------------------------------------------------------
 // ae::AudioData class
 //------------------------------------------------------------------------------
 class AudioData
@@ -13016,7 +13100,342 @@ void DebugCamera::m_Precalculate()
 }
 
 //------------------------------------------------------------------------------
-// Helpers
+// ae::CollisionMesh member functions
+//------------------------------------------------------------------------------
+CollisionMesh::CollisionMesh( ae::Tag tag ) :
+  m_vertices( tag ),
+  m_indices( tag )
+{
+  Clear();
+}
+
+void CollisionMesh::Load( Params params )
+{
+  AE_ASSERT_MSG( !m_vertices.Length(), "TODO! Should allow multiple Load()s without clearing" );
+  
+  AE_ASSERT( params.positions );
+  AE_ASSERT_MSG( params.positionStride > sizeof(float) * 3, "Must specify the number of bytes between each position" );
+  if ( params.indexCount )
+  {
+    AE_ASSERT( params.indexCount % 3 == 0 );
+  }
+  else
+  {
+    AE_ASSERT( params.vertexCount % 3 == 0 );
+  }
+
+  bool identityTransform = ( params.transform == ae::Matrix4::Identity() );
+
+  ae::Vec3 p0 = params.positions[ 0 ];
+  if ( !identityTransform )
+  {
+    p0 = ( params.transform * ae::Vec4( p0, 1.0f ) ).GetXYZ();
+  }
+  m_aabb = ae::AABB( p0, p0 );
+
+  m_vertices.Reserve( params.vertexCount );
+  for ( uint32_t i = 0; i < params.vertexCount; i++ )
+  {
+    Vertex vert;
+    memset( &vert, 0, sizeof(vert) );
+    
+    vert.position = ae::Vec4( *params.positions, 1.0f );
+    if ( !identityTransform )
+    {
+      vert.position = params.transform * vert.position;
+    }
+    params.positions = (ae::Vec3*)( (uint8_t*)params.positions + params.positionStride );
+    
+    m_aabb.Expand( vert.position.GetXYZ() );
+    m_vertices.Append( vert );
+  }
+  
+  m_indices.Append( params.indices16, params.indexCount );
+}
+
+void CollisionMesh::Clear()
+{
+  m_vertices.Clear();
+  m_indices.Clear();
+  m_aabb = ae::AABB();
+}
+
+bool CollisionMesh::Raycast( const RaycastParams& params, RaycastResult* outResult ) const
+{
+  // Early out for parameters that will give no results
+  if ( params.maxLength < 0.0f || params.maxHits == 0 )
+  {
+    return false;
+  }
+  
+  bool limitRay = params.maxLength != 0.0f;
+  ae::Vec3 normParamsDir = params.direction.SafeNormalizeCopy();
+
+  // Obb in world space
+  {
+    float obbDistance = ae::MaxValue< float >();
+    ae::OBB obb( params.transform * m_aabb.GetTransform() );
+    if ( !obb.IntersectRay( params.source, normParamsDir, nullptr, &obbDistance )
+      || ( limitRay && obbDistance > params.maxLength ) )
+    {
+      if ( ae::DebugLines* debug = params.debug )
+      {
+        ae::Vec3 rayEnd = params.source + normParamsDir * ( limitRay ? params.maxLength : 1000.0f );
+        debug->AddLine( params.source, rayEnd, params.debugColor );
+      }
+      return false; // Early out if ray doesn't touch obb
+    }
+    
+    if ( ae::DebugLines* debug = params.debug )
+    {
+      // Ray intersects obb
+      debug->AddOBB( obb.GetTransform(), params.debugColor );
+    }
+  }
+  
+  const ae::Matrix4 invTransform = params.transform.GetInverse();
+  const ae::Vec3 source( invTransform * ae::Vec4( params.source, 1.0f ) );
+  const ae::Vec3 ray( invTransform * ae::Vec4( normParamsDir * ( limitRay ? params.maxLength : 1.0f ), 0.0f ) );
+  const ae::Vec3 normDir = ray.SafeNormalizeCopy();
+  const bool ccw = params.hitCounterclockwise;
+  const bool cw = params.hitClockwise;
+  
+  const uint32_t triCount = m_indices.Length() / 3;
+  const Index* indices = m_indices.Begin();
+  const Vertex* vertices = &m_vertices[ 0 ];
+
+  uint32_t hitCount  = 0;
+  RaycastResult::Hit hits[ countof(RaycastResult::hits) + 1 ];
+  const uint32_t maxHits = ae::Min( params.maxHits, countof(RaycastResult::hits) );
+  for ( uint32_t i = 0; i < triCount; i++ )
+  {
+    ae::Vec3 p, n;
+    ae::Vec3 a = vertices[ indices[ i * 3 ] ].position.GetXYZ();
+    ae::Vec3 b = vertices[ indices[ i * 3 + 1 ] ].position.GetXYZ();
+    ae::Vec3 c = vertices[ indices[ i * 3 + 2 ] ].position.GetXYZ();
+    if ( IntersectRayTriangle( source, ray, a, b, c, limitRay, ccw, cw, &p, &n, nullptr ) )
+    {
+      RaycastResult::Hit& outHit = hits[ hitCount ];
+      hitCount++;
+      AE_ASSERT( hitCount <= maxHits + 1 ); // Allow one extra hit, then sort and remove last hit below
+      
+      outHit.position = ae::Vec3( params.transform * ae::Vec4( p, 1.0f ) );
+      outHit.normal = ae::Vec3( params.transform * ae::Vec4( n, 0.0f ) );
+      outHit.distance = ( outHit.position - params.source ).Length(); // Calculate here because transform might not have uniform scale
+      outHit.userData = params.userData;
+      
+      if ( hitCount > maxHits )
+      {
+        std::sort( hits, hits + hitCount, []( const RaycastResult::Hit& a, const RaycastResult::Hit& b ) { return a.distance < b.distance; } );
+        hitCount = maxHits;
+      }
+    }
+  }
+  
+  if ( ae::DebugLines* debug = params.debug )
+  {
+    ae::Vec3 rayEnd;
+    if ( !limitRay )
+    {
+      if ( hitCount )
+      {
+        rayEnd = hits[ hitCount - 1 ].position;
+      }
+      else
+      {
+        rayEnd = params.source + normParamsDir * 1000.0f;
+      }
+    }
+    else
+    {
+      rayEnd = params.source + normParamsDir * params.maxLength;
+    }
+    debug->AddLine( params.source, rayEnd, params.debugColor );
+    
+    for ( uint32_t i = 0; i < hitCount; i++ )
+    {
+      const RaycastResult::Hit* hit = &hits[ i ];
+      const ae::Vec3 p = hit->position;
+      const ae::Vec3 n = hit->normal;
+      float s = ( hitCount > 1 ) ? ( i / ( hitCount - 1.0f ) ) : 1.0f;
+      debug->AddCircle( p, n, ae::Lerp( 0.25f, 0.3f, s ), params.debugColor, 8 );
+      debug->AddLine( p, p + n, params.debugColor );
+    }
+  }
+  
+  if ( outResult )
+  {
+    std::sort( hits, hits + hitCount, []( const RaycastResult::Hit& a, const RaycastResult::Hit& b ) { return a.distance < b.distance; } );
+    outResult->hitCount = hitCount;
+    for ( uint32_t i = 0; i < hitCount; i++ )
+    {
+      hits[ i ].normal.SafeNormalize();
+      outResult->hits[ i ] = hits[ i ];
+    }
+  }
+  return hitCount;
+}
+
+CollisionMesh::RaycastResult CollisionMesh::Raycast( const RaycastParams& params, const RaycastResult& prevResult ) const
+{
+  // @TODO: For params requesting a single hit set max length of ray to prevResult hit distance for obb early out
+  RaycastResult nextResult;
+  if ( Raycast( params, &nextResult ) )
+  {
+    CollisionMesh::RaycastResult::Accumulate( params, prevResult, &nextResult );
+    return nextResult;
+  }
+  return prevResult;
+}
+
+CollisionMesh::PushOutInfo CollisionMesh::PushOut( const PushOutParams& params, const PushOutInfo& prevInfo ) const
+{
+  if ( ae::DebugLines* debug = params.debug )
+  {
+    debug->AddSphere( prevInfo.sphere.center, prevInfo.sphere.radius, params.debugColor, 8 );
+  }
+
+  ae::OBB obb( params.transform * m_aabb.GetTransform() );
+  if ( obb.GetSignedDistanceFromSurface( prevInfo.sphere.center ) > prevInfo.sphere.radius )
+  {
+    return prevInfo; // Early out if sphere is to far from mesh
+  }
+  
+  if ( ae::DebugLines* debug = params.debug )
+  {
+    // Sphere intersects obb
+    debug->AddOBB( obb.GetTransform(), params.debugColor );
+  }
+  
+  PushOutInfo result;
+  result.sphere = prevInfo.sphere;
+  result.velocity = prevInfo.velocity;
+  bool hasIdentityTransform = ( params.transform == ae::Matrix4::Identity() );
+  
+  const uint32_t triCount = m_indices.Length() / 3;
+  const Index* indices = m_indices.Begin();
+  const Vertex* vertices = &m_vertices[ 0 ];
+  
+  for ( uint32_t i = 0; i < triCount; i++ )
+  {
+    ae::Vec3 a, b, c;
+    if ( hasIdentityTransform )
+    {
+      a = vertices[ indices[ i * 3 ] ].position.GetXYZ();
+      b = vertices[ indices[ i * 3 + 1 ] ].position.GetXYZ();
+      c = vertices[ indices[ i * 3 + 2 ] ].position.GetXYZ();
+    }
+    else
+    {
+      a = ae::Vec3( params.transform * vertices[ indices[ i * 3 ] ].position );
+      b = ae::Vec3( params.transform * vertices[ indices[ i * 3 + 1 ] ].position );
+      c = ae::Vec3( params.transform * vertices[ indices[ i * 3 + 2 ] ].position );
+    }
+    
+    ae::Vec3 triNormal = ( ( b - a ).Cross( c - a ) ).SafeNormalizeCopy();
+    ae::Vec3 triCenter( ( a + b + c ) / 3.0f );
+    
+    ae::Vec3 triToSphereDir = ( result.sphere.center - triCenter );
+    if ( triNormal.Dot( triToSphereDir ) < 0.0f )
+    {
+      continue;
+    }
+    
+    ae::Vec3 triHitPos;
+    if ( result.sphere.IntersectTriangle( a, b, c, &triHitPos ) )
+    {
+      triToSphereDir = ( result.sphere.center - triHitPos );
+      if ( triNormal.Dot( triToSphereDir ) < 0.0f )
+      {
+        continue;
+      }
+      
+      ae::Vec3 closestSpherePoint = ( triHitPos - result.sphere.center ).SafeNormalizeCopy();
+      closestSpherePoint *= result.sphere.radius;
+      closestSpherePoint += result.sphere.center;
+      
+      result.sphere.center += triHitPos - closestSpherePoint;
+      result.velocity.ZeroDirection( -triNormal );
+      
+      // @TODO: Sort. Shouldn't randomly discard hits.
+      if ( result.hits.Length() < result.hits.Size() )
+      {
+        result.hits.Append( { triHitPos, triNormal } );
+      }
+      
+      if ( ae::DebugLines* debug = params.debug )
+      {
+        debug->AddLine( a, b, params.debugColor );
+        debug->AddLine( b, c, params.debugColor );
+        debug->AddLine( c, a, params.debugColor );
+        
+        debug->AddLine( triHitPos, triHitPos + triNormal * 2.0f, params.debugColor );
+        debug->AddSphere( triHitPos, 0.05f, params.debugColor, 4 );
+      }
+    }
+  }
+  
+  if ( result.hits.Length() )
+  {
+    PushOutInfo::Accumulate( params, prevInfo, &result );
+    return result;
+  }
+  else
+  {
+    return prevInfo;
+  }
+}
+
+//------------------------------------------------------------------------------
+// ae::CollisionMesh::RaycastResult member functions
+//------------------------------------------------------------------------------
+void CollisionMesh::RaycastResult::Accumulate( const RaycastParams& params, const RaycastResult& prev, RaycastResult* next )
+{
+  uint32_t accumHitCount = 0;
+  Hit accumHits[ countof(next->hits) * 2 ];
+  
+  for ( uint32_t i = 0; i < next->hitCount; i++ )
+  {
+    accumHits[ accumHitCount ] = next->hits[ i ];
+    accumHitCount++;
+  }
+  for ( uint32_t i = 0; i < prev.hitCount; i++ )
+  {
+    accumHits[ accumHitCount ] = prev.hits[ i ];
+    accumHitCount++;
+  }
+  std::sort( accumHits, accumHits + accumHitCount, []( const Hit& h0, const Hit& h1 ){ return h0.distance < h1.distance; } );
+  
+  next->hitCount = ae::Min( accumHitCount, params.maxHits, countof(next->hits) );
+  for ( uint32_t i = 0; i < next->hitCount; i++ )
+  {
+    next->hits[ i ] = accumHits[ i ];
+  }
+}
+
+//------------------------------------------------------------------------------
+// ae::CollisionMesh::PushOutInfo member functions
+//------------------------------------------------------------------------------
+void CollisionMesh::PushOutInfo::Accumulate( const PushOutParams& params, const PushOutInfo& prev, PushOutInfo* next )
+{
+  // @NOTE: Leave next::position/velocity unchanged since it's the latest
+  // @TODO: Params are currently not used, but they could be used for sorting later
+  auto&& nHits = next->hits;
+  for ( auto&& hit : prev.hits )
+  {
+    if ( nHits.Length() < nHits.Size() )
+    {
+      nHits.Append( hit );
+    }
+    else
+    {
+      break;
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+// ae::Audio Helpers
 //------------------------------------------------------------------------------
 void _CheckALError()
 {
