f5eefa60be710b27da922360e40ce5d49f3a6e87
John Hughes <hughesj17@gmail.com>
2022-02-15 10:53:49 -0800

    More file loading wip

--- example/14_Emscripten/14_Emscripten.cpp
+++ example/14_Emscripten/14_Emscripten.cpp
@@ -48,12 +48,13 @@ struct Vertex
 };
 
 Vertex kTriangleVerts[] = {
-	{ ae::Vec4( -0.5f, -0.4f, 0.0f, 1.0f ), ae::Color::PicoRed().GetLinearRGBA(), ae::Vec2( 0.0f, 0.0f ) },
-	{ ae::Vec4( 0.5f, -0.4f, 0.0f, 1.0f ), ae::Color::PicoGreen().GetLinearRGBA(), ae::Vec2( 1.0f, 0.0f ) },
-	{ ae::Vec4( 0.0f, 0.4f, 0.0f, 1.0f ), ae::Color::PicoBlue().GetLinearRGBA(), ae::Vec2( 0.0f, 1.0f ) },
+	{ ae::Vec4( -0.5f, -0.5f, 0.0f, 1.0f ), ae::Color::PicoRed().GetLinearRGBA(), ae::Vec2( 0.0f, 0.0f ) },
+	{ ae::Vec4( 0.5f, -0.5f, 0.0f, 1.0f ), ae::Color::PicoGreen().GetLinearRGBA(), ae::Vec2( 1.0f, 0.0f ) },
+	{ ae::Vec4( -0.5f, 0.5f, 0.0f, 1.0f ), ae::Color::PicoBlue().GetLinearRGBA(), ae::Vec2( 0.0f, 1.0f ) },
+	{ ae::Vec4( 0.5f, 0.5f, 0.0f, 1.0f ), ae::Color::PicoBlue().GetLinearRGBA(), ae::Vec2( 1.0f, 1.0f ) },
 };
 
-uint16_t kTriangleIndices[] = { 0, 1, 2 };
+uint16_t kTriangleIndices[] = { 0, 1, 2, 3, 1, 2 };
 
 typedef struct Texture
 {
@@ -108,76 +109,209 @@ static Texture* find_or_cache_url( const char* url )
 	return 0; // fail
 }
 
-struct LoadFileInfo
+//------------------------------------------------------------------------------
+// File forward declarations
+//------------------------------------------------------------------------------
+extern "C" {
+void _AsyncFileLoader_LoadSuccess( void* arg, void* data, uint32_t length );
+void _AsyncFileLoader_LoadFail( void* arg, uint32_t code, bool timeout );
+}
+
+//------------------------------------------------------------------------------
+// LoadFileInfo class
+//------------------------------------------------------------------------------
+class LoadFileInfo
 {
-	bool finished = false;
-	uint8_t* data = nullptr; // Null terminated for convenience
-	uint32_t length = 0;
-	uint32_t status = 0;
+public:
+	enum class Status
+	{
+		Success,
+		Pending,
+		NotFound,
+		Timeout,
+		Error
+	};
+
+	const char* GetUrl() const;
+	Status GetStatus() const;
+	//! Platform specific error code eg. 200, 404, etc. for http
+	uint32_t GetCode() const;
+	//! Null terminated for convenience
+	const void* GetData() const;
+	uint32_t GetLength() const;
+	float GetElapsedTime() const;
+
+private:
+	friend void _AsyncFileLoader_LoadSuccess( void* arg, void* data, uint32_t length );
+	friend void _AsyncFileLoader_LoadFail( void* arg, uint32_t code, bool timeout );
+	friend class AsyncFileLoader;
+	ae::Str256 m_url;
+	uint8_t* m_data = nullptr;
+	uint32_t m_length = 0;
+	Status m_status = Status::Pending;
+	uint32_t m_code = 0;
+	double m_startTime = 0.0;
+	double m_finishTime = 0.0;
 };
 
-extern "C" {
-void EMSCRIPTEN_KEEPALIVE handle_file_load_success( void* arg, void* data, uint32_t length )
+//------------------------------------------------------------------------------
+// AsyncFileLoader class
+//------------------------------------------------------------------------------
+class AsyncFileLoader
 {
-	LoadFileInfo* info = (LoadFileInfo*)arg;
-	info->data = (uint8_t*)ae::Allocate( TAG_ALL, length + 1, 8 );
-	memcpy( info->data, data, length );
-	info->data[ length ] = 0;
-	info->length = length;
-	info->finished = true;
+public:
+	//! Loads a file asynchronously. Returns a LoadFileInfo object to be freed
+	//! later with AsyncFileLoader::Destroy(). A zero or negative /p timeoutSec
+	//! value will disable the timeout.
+	const LoadFileInfo* Load( const char* url, float timeoutSec );
+	void Destroy( const LoadFileInfo* info );
+
+	const LoadFileInfo* GetFile( uint32_t idx ) const;
+	uint32_t GetFileCount() const;
+
+private:
+	ae::Array< LoadFileInfo* > m_files = AE_ALLOC_TAG_FILE;
+};
+
+//------------------------------------------------------------------------------
+// LoadFileInfo member functions
+//------------------------------------------------------------------------------
+const char* LoadFileInfo::GetUrl() const
+{
+	return m_url.c_str();
+}
+LoadFileInfo::Status LoadFileInfo::GetStatus() const
+{
+	return m_status;
+}
+uint32_t LoadFileInfo::GetCode() const
+{
+	return m_code;
+}
+const void* LoadFileInfo::GetData() const
+{
+	return m_data;
+}
+uint32_t LoadFileInfo::GetLength() const
+{
+	return m_length;
+}
+float LoadFileInfo::GetElapsedTime() const
+{
+	return m_finishTime ? ( m_finishTime - m_startTime ) : ( ae::GetTime() - m_startTime );
 }
 
-void EMSCRIPTEN_KEEPALIVE handle_file_load_fail( void* arg, uint32_t status )
+//------------------------------------------------------------------------------
+// AsyncFileLoader implementation
+//------------------------------------------------------------------------------
+extern "C" void EMSCRIPTEN_KEEPALIVE _AsyncFileLoader_LoadSuccess( void* arg, void* data, uint32_t length )
 {
 	LoadFileInfo* info = (LoadFileInfo*)arg;
-	info->status = status;
-	info->finished = true;
-}
+	info->m_finishTime = ae::GetTime();
+	info->m_data = (uint8_t*)ae::Allocate( TAG_ALL, length + 1, 8 );
+	memcpy( info->m_data, data, length );
+	info->m_data[ length ] = 0;
+	info->m_length = length;
+
+	info->m_status = LoadFileInfo::Status::Success;
+	info->m_code = 200;
 }
 
-typedef void (file_load_async_success_fn)( void*, void*, uint32_t );
-typedef void (file_load_async_fail_fn)( void*, uint32_t );
-EM_JS( void, file_load_async_impl, ( const char* url, void* arg, uint32_t timeoutMs, file_load_async_success_fn* onload, file_load_async_fail_fn onerror ),
+extern "C" void EMSCRIPTEN_KEEPALIVE _AsyncFileLoader_LoadFail( void* arg, uint32_t code, bool timeout )
+{
+	LoadFileInfo* info = (LoadFileInfo*)arg;
+	info->m_finishTime = ae::GetTime();
+	info->m_code = code;
+	if ( timeout )
+	{
+		info->m_status = LoadFileInfo::Status::Timeout;
+	}
+	else
+	{
+		switch ( code )
+		{
+			case 404:
+				info->m_status = LoadFileInfo::Status::NotFound;
+				break;
+			default:
+				info->m_status = LoadFileInfo::Status::Error;
+				break;
+		}
+	}
+}
+
+EM_JS( void, _AsyncFileLoader_LoadImpl, ( const char* url, void* arg, uint32_t timeoutMs ),
 {
 	var xhr = new XMLHttpRequest();
 	xhr.timeout = timeoutMs;
 	xhr.open('GET', UTF8ToString(url), true);
 	xhr.responseType = 'arraybuffer';
 	xhr.ontimeout = function xhr_ontimeout() {
-		_handle_file_load_fail(arg, xhr.status);
+		__AsyncFileLoader_LoadFail(arg, xhr.status, true);
 	};
 	xhr.onload = function xhr_onload() {
-		if (xhr.status == 200 && xhr.response) {
-			var byteArray = new Uint8Array(xhr.response);
-			var buffer = _malloc(byteArray.length);
-			HEAPU8.set(byteArray, buffer);
-			_handle_file_load_success(arg, buffer, byteArray.length);
-			_free(buffer);
-		} else {
-			_handle_file_load_fail(arg, xhr.status);
+		if (xhr.status == 200) {
+			if (xhr.response) {
+				var byteArray = new Uint8Array(xhr.response);
+				var buffer = _malloc(byteArray.length);
+				HEAPU8.set(byteArray, buffer);
+				__AsyncFileLoader_LoadSuccess(arg, buffer, byteArray.length);
+				_free(buffer);
+			}
+			else {
+				__AsyncFileLoader_LoadSuccess(arg, 0, 0); // Empty response but request succeeded
+			}
+			
 		}
 	};
 	xhr.onerror = function xhrError() {
-		_handle_file_load_fail(arg, xhr.status);
+		__AsyncFileLoader_LoadFail(arg, xhr.status, false);
 	};
 	xhr.send(null);
 } );
-LoadFileInfo* file_load_async( const char* url, uint32_t timeoutMs, file_load_async_success_fn* onload, file_load_async_fail_fn onerror )
+
+const LoadFileInfo* AsyncFileLoader::Load( const char* url, float timeoutSec )
 {
+	double t = ae::GetTime();
 	LoadFileInfo* info = ae::New< LoadFileInfo >( TAG_ALL );
-	file_load_async_impl( url, info, timeoutMs, onload, onerror );
+	info->m_url = url;
+	info->m_startTime = t;
+	uint32_t timeoutMs;
+	if ( timeoutSec <= 0.0f )
+	{
+		timeoutMs = 0.0f;
+	}
+	else
+	{
+		timeoutMs = timeoutSec * 1000.0f;
+		timeoutMs = ae::Max( 1u, timeoutMs ); // Prevent rounding down to infinite timeout
+	}
+	_AsyncFileLoader_LoadImpl( url, info, timeoutMs );
 	return info;
 }
 
-void file_free( LoadFileInfo* info )
+void AsyncFileLoader::Destroy( const LoadFileInfo* info )
 {
 	if ( info )
 	{
-		ae::Free( info->data );
+		ae::Free( info->m_data );
 		ae::Delete( info );
 	}
 }
 
+const LoadFileInfo* AsyncFileLoader::GetFile( uint32_t idx ) const
+{
+	return m_files[ idx ];
+}
+
+uint32_t AsyncFileLoader::GetFileCount() const
+{
+	return m_files.Length();
+}
+
+//------------------------------------------------------------------------------
+// Game class
+//------------------------------------------------------------------------------
 class Game
 {
 public:
@@ -185,7 +319,9 @@ public:
 	ae::Vec2 charVel = ae::Vec2( 0.0f );
 	float rotation = 0.0f;
 	ae::Texture2D m_texture;
-	LoadFileInfo* m_info = nullptr;
+
+	AsyncFileLoader fileLoader;
+	const LoadFileInfo* m_info = nullptr;
 
 	Game() : charPos( 0.0f ), charVel( 0.0f ), rotation( 0.0f ) {}
 
@@ -207,7 +343,7 @@ public:
 		m_texture.Initialize( &data, 1, 1, ae::Texture::Format::R8, ae::Texture::Type::Uint8, ae::Texture::Filter::Nearest, ae::Texture::Wrap::Repeat, false );
 		find_or_cache_url( "moon.png" );
 
-		m_info = file_load_async( "test.txt", 2500, handle_file_load_success, handle_file_load_fail );
+		m_info = fileLoader.Load( "https://raw.githubusercontent.com/johnhues/aether-game-utils/master/LICENSE", 2.1f );
 	}
 
 	void Update( float dt )
@@ -225,17 +361,26 @@ public:
 			s_first = false;
 		}
 
-		if ( m_info && m_info->finished )
+		if ( m_info && m_info->GetStatus() != LoadFileInfo::Status::Pending )
 		{
-			if ( m_info->data )
+			AE_INFO( "finish time: #s", m_info->GetElapsedTime() );
+			switch ( m_info->GetStatus() )
 			{
-				AE_INFO( "loaded file. '#' length: #", (const char*)m_info->data, m_info->length );
+				case LoadFileInfo::Status::Success:
+					AE_INFO( "Loaded file: '#' Length: # (#)", m_info->GetUrl(), m_info->GetLength(), m_info->GetCode() );
+					printf( "%s\n", (const char*)m_info->GetData() );
+					break;
+				case LoadFileInfo::Status::NotFound:
+					AE_ERR( "File not found: '#' (#)", m_info->GetUrl(), m_info->GetCode() );
+					break;
+				case LoadFileInfo::Status::Timeout:
+					AE_ERR( "File load timeout: '#' (#)", m_info->GetUrl(), m_info->GetCode() );
+					break;
+				default:
+					AE_ERR( "Error loading file: '#' (#)", m_info->GetUrl(), m_info->GetCode() );
+					break;
 			}
-			else
-			{
-				AE_INFO( "failed to load file. status: #", m_info->status );
-			}
-			file_free( m_info );
+			fileLoader.Destroy( m_info );
 			m_info = nullptr;
 		}
 
@@ -261,7 +406,7 @@ public:
 	void Render( float dt )
 	{
 		gfx.Activate();
-		gfx.Clear( ae::Color::PicoDarkPurple() );
+		gfx.Clear( ae::Color::Black() );
 
 		ae::Matrix4 transform = ae::Matrix4::Scaling( ae::Vec3( 1.0f / gfx.GetAspectRatio(), 1.0f, 1.0f ) );
 		transform *= ae::Matrix4::Translation( ae::Vec3( charPos, 0.0f ) );
