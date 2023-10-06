90d76eb1e43a77aadeeb77658fd9ff1efdc444f2
John Hughes <hughesj17@gmail.com>
2023-10-05 22:19:09 -0700

    Shared library cmake utility and example

--- AddBundle.cmake
+++ AddBundle.cmake
@@ -1,7 +1,37 @@
 cmake_minimum_required(VERSION 3.24 FATAL_ERROR)
 
+function(ae_add_shared_library _AE_NAME _AE_MAJOR_MINOR_PATCH_VERSION _AE_SRC_FILES _AE_HEADER_FILES _AE_LIBS _AE_PUBLIC_INCLUDE_DIRS _AE_PRIVATE_INCLUDE_DIRS)
+	message(STATUS "_AE_NAME \"${_AE_NAME}\" \(${CMAKE_BUILD_TYPE}\)")
+	message(STATUS "_AE_MAJOR_MINOR_PATCH_VERSION \"${_AE_MAJOR_MINOR_PATCH_VERSION}\"")
+	message(STATUS "_AE_SRC_FILES \"${_AE_SRC_FILES}\"")
+	message(STATUS "_AE_HEADER_FILES \"${_AE_HEADER_FILES}\"")
+	message(STATUS "_AE_LIBS \"${_AE_LIBS}\"")
+	message(STATUS "_AE_PUBLIC_INCLUDE_DIRS \"${_AE_PUBLIC_INCLUDE_DIRS}\"")
+	message(STATUS "_AE_PRIVATE_INCLUDE_DIRS \"${_AE_PRIVATE_INCLUDE_DIRS}\"")
+	message(STATUS "")
+
+	add_library(${_AE_NAME} SHARED ${_AE_SRC_FILES})
+	if(_AE_LIBS)
+		target_link_libraries(${_AE_NAME} ${_AE_LIBS})
+	endif()
+	if(_AE_INCLUDE_DIRS)
+		target_include_directories(${_AE_NAME} PRIVATE ${_AE_INCLUDE_DIRS})
+	endif()
+	if (("${CMAKE_GENERATOR}" STREQUAL "Xcode"))
+		set(_AE_APPLE_SKIP_INSTALL "YES") # See Professional CMake 24.7. Creating And Exporting Archives
+	endif()
+	if(APPLE)
+		set_target_properties(${_AE_NAME} PROPERTIES
+			FRAMEWORK TRUE
+			FRAMEWORK_VERSION "${_AE_MAJOR_MINOR_PATCH_VERSION}"
+			PUBLIC_HEADER "${_AE_HEADER_FILES}"
+			XCODE_ATTRIBUTE_SKIP_INSTALL "${_AE_APPLE_SKIP_INSTALL}" # See Professional CMake 24.7. Creating And Exporting Archives
+		)
+	endif()
+endfunction()
+
 # Bundle helper
-function(add_bundle _AE_BUNDLE_NAME _AE_EXECUTABLE_NAME _AE_BUNDLE_ID _AE_APPLE_DEVELOPMENT_TEAM _AE_MAJOR_MINOR_PATCH_VERSION _AE_ICNS_FILE _AE_SRC_FILES _AE_RESOURCES _AE_LIBS _AE_INCLUDE_DIRS)
+function(ae_add_bundle _AE_BUNDLE_NAME _AE_EXECUTABLE_NAME _AE_BUNDLE_ID _AE_APPLE_DEVELOPMENT_TEAM _AE_MAJOR_MINOR_PATCH_VERSION _AE_ICNS_FILE _AE_SRC_FILES _AE_RESOURCES _AE_LIBS _AE_PACKAGE_LIBS _AE_INCLUDE_DIRS)
 	message(STATUS "_AE_BUNDLE_NAME \"${_AE_BUNDLE_NAME}\" \(${CMAKE_BUILD_TYPE})")
 	message(STATUS "_AE_EXECUTABLE_NAME \"${_AE_EXECUTABLE_NAME}\"")
 	message(STATUS "_AE_BUNDLE_ID \"${_AE_BUNDLE_ID}\"")
@@ -11,6 +41,7 @@ function(add_bundle _AE_BUNDLE_NAME _AE_EXECUTABLE_NAME _AE_BUNDLE_ID _AE_APPLE_
 	message(STATUS "_AE_SRC_FILES \"${_AE_SRC_FILES}\"")
 	message(STATUS "_AE_RESOURCES \"${_AE_RESOURCES}\"")
 	message(STATUS "_AE_LIBS \"${_AE_LIBS}\"")
+	message(STATUS "_AE_PACKAGE_LIBS \"${_AE_PACKAGE_LIBS}\"")
 	message(STATUS "_AE_INCLUDE_DIRS \"${_AE_INCLUDE_DIRS}\"")
 	message(STATUS "")
 
@@ -22,10 +53,13 @@ function(add_bundle _AE_BUNDLE_NAME _AE_EXECUTABLE_NAME _AE_BUNDLE_ID _AE_APPLE_
 	endif()
 
 	add_executable(${_AE_EXECUTABLE_NAME} ${_AE_EXE_TYPE} ${_AE_SRC_FILES})
-	if (_AE_LIBS)
+	if(_AE_LIBS)
 		target_link_libraries(${_AE_EXECUTABLE_NAME} ${_AE_LIBS})
 	endif()
-	if (_AE_INCLUDE_DIRS)
+	if(_AE_PACKAGE_LIBS)
+		target_link_libraries(${_AE_EXECUTABLE_NAME} ${_AE_PACKAGE_LIBS})
+	endif()
+	if(_AE_INCLUDE_DIRS)
 		target_include_directories(${_AE_EXECUTABLE_NAME} PRIVATE ${_AE_INCLUDE_DIRS})
 	endif()
 
@@ -42,18 +76,15 @@ function(add_bundle _AE_BUNDLE_NAME _AE_EXECUTABLE_NAME _AE_BUNDLE_ID _AE_APPLE_
 			get_filename_component(resource_path ${resource} ABSOLUTE) # First get absolute path to resource
 			file(RELATIVE_PATH resource_path ${CMAKE_CURRENT_SOURCE_DIR} ${resource_path}) # Get the relative path to the resource from the root of the resource
 			cmake_path(GET resource_path PARENT_PATH resource_path) # Remove file name from path
-			set_source_files_properties(${resource} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/${resource_path})
+			set_source_files_properties(${resource} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources/${resource_path}")
 		endforeach()
 
-		set(CMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH "" CACHE STRING "") # See Professional CMake 24.8.1. Device-only Bundles
-		set(CMAKE_OSX_ARCHITECTURES "$(ARCHS_STANDARD)" CACHE STRING "") # See Professional CMake 24.8.1. Device-only Bundles
-
-		if (("${CMAKE_GENERATOR}" STREQUAL "Xcode"))
+		if ("${CMAKE_GENERATOR}" STREQUAL "Xcode")
 			if (_AE_APPLE_DEVELOPMENT_TEAM)
 				set(_AE_APPLE_CODE_SIGN_IDENTITY "Apple Development") # See Professional CMake 24.6.1. Signing Identity And Development Team
 			endif()
-			set(_AE_APPLE_INSTALL_PATH "$(LOCAL_APPS_DIR)") # See Professional 24.7. Creating And Exporting Archives
-			set(_AE_APPLE_SKIP_INSTALL "NO") # See Professional 24.7. Creating And Exporting Archives
+			set(_AE_APPLE_INSTALL_PATH "$(LOCAL_APPS_DIR)") # See Professional CMake 24.7. Creating And Exporting Archives
+			set(_AE_APPLE_SKIP_INSTALL "NO") # See Professional CMake 24.7. Creating And Exporting Archives
 		endif()
 
 		set_target_properties(${_AE_EXECUTABLE_NAME} PROPERTIES
@@ -68,24 +99,28 @@ function(add_bundle _AE_BUNDLE_NAME _AE_EXECUTABLE_NAME _AE_BUNDLE_ID _AE_APPLE_
 			XCODE_ATTRIBUTE_INFOPLIST_KEY_LSApplicationCategoryType "public.app-category.games"
 			# XCODE_ATTRIBUTE_INFOPLIST_KEY_CFBundleIconFile "${_AE_ICNS_FILE}"
 			XCODE_ATTRIBUTE_INFOPLIST_KEY_NSPrincipalClass "NSApplication"
-			# XCODE_ATTRIBUTE_INFOPLIST_KEY_NSHumanReadableCopyright "(c)2023 Example Co." @TODO
+			# XCODE_ATTRIBUTE_INFOPLIST_KEY_NSHumanReadable @TODO
 
 			XCODE_ATTRIBUTE_DEVELOPMENT_TEAM "${_AE_APPLE_DEVELOPMENT_TEAM}"
 			XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "${_AE_APPLE_CODE_SIGN_IDENTITY}"
 
-			XCODE_ATTRIBUTE_INSTALL_PATH "${_AE_APPLE_INSTALL_PATH}" # See Professional 24.7. Creating And Exporting Archives
-			XCODE_ATTRIBUTE_SKIP_INSTALL "${_AE_APPLE_SKIP_INSTALL}" # See Professional 24.7. Creating And Exporting Archives
+			XCODE_ATTRIBUTE_INSTALL_PATH "${_AE_APPLE_INSTALL_PATH}" # See Professional CMake 24.7. Creating And Exporting Archives
+			XCODE_ATTRIBUTE_SKIP_INSTALL "${_AE_APPLE_SKIP_INSTALL}" # See Professional CMake 24.7. Creating And Exporting Archives
 
 			XCODE_ATTRIBUTE_ENABLE_HARDENED_RUNTIME "YES"
 			XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH "No"
 
-			RESOURCE "${_AE_RESOURCES}"
 			MACOSX_BUNDLE_ICON_FILE "${_AE_ICNS_FILE}" # CFBundleIconFile (*.icns file path)
 
 			OUTPUT_NAME "${_AE_BUNDLE_NAME}"
 
+			MACOSX_RPATH TRUE
 			INSTALL_RPATH @executable_path/../Frameworks
 		)
+
+		if(_AE_PACKAGE_LIBS)
+			set_target_properties(${_AE_EXECUTABLE_NAME} PROPERTIES XCODE_EMBED_FRAMEWORKS "${_AE_PACKAGE_LIBS}") # 24.10. Embedding Frameworks, Plugins And Extensions
+		endif()
 	elseif(EMSCRIPTEN)
 		set(_AE_EM_LINKER_FLAGS
 			"-lopenal"
---
+++ examples/23_SharedLibrary.cpp
@@ -0,0 +1,60 @@
+//------------------------------------------------------------------------------
+// 23_SharedLibrary.cpp
+//------------------------------------------------------------------------------
+// Copyright (c) 2023 John Hughes
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//------------------------------------------------------------------------------
+// Headers
+//------------------------------------------------------------------------------
+#include "aether.h"
+
+//------------------------------------------------------------------------------
+// Library entry point
+//------------------------------------------------------------------------------
+int Run()
+{
+	AE_LOG( "Initialize" );
+	ae::Window window;
+	ae::GraphicsDevice graphicsDevice;
+	ae::Input input;
+	ae::TimeStep timeStep;
+	
+	window.Initialize( 800, 600, false, true );
+	window.SetTitle( "Shared Library" );
+	graphicsDevice.Initialize( &window );
+	input.Initialize( &window );
+	timeStep.SetTimeStep( 1.0f / 60.0f );
+
+	while ( !input.quit )
+	{
+		input.Pump();
+		graphicsDevice.Activate();
+		graphicsDevice.Clear( ae::Color::PicoDarkPurple() );
+		graphicsDevice.Present();
+		timeStep.Tick();
+	}
+
+	AE_LOG( "Terminate" );
+	input.Terminate();
+	graphicsDevice.Terminate();
+	window.Terminate();
+
+	return 0;
+}
